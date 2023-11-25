a591bf9dfef4fe5f8cd5c99cf3894860f763d9e2
TepMan <tepper.andreas@gmail.com>
2023-11-25 21:13:47 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,59 @@
+let Ergebnis = 0
+input.onButtonPressed(Button.A, function () {
+    Ergebnis = randint(1, 6)
+    if (Ergebnis == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Ergebnis == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Ergebnis == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Ergebnis == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Ergebnis == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Ergebnis == 6) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    } else {
+        basic.showIcon(IconNames.Ghost)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Ergebnis = randint(1, 6)
+    basic.showNumber(Ergebnis)
+})
